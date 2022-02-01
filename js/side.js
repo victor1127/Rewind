e0d39adf9c5c6848d65492a25542b6002bc137ec
victor1127 <95936106+victor1127@users.noreply.github.com>
2022-02-01 18:40:02 -0400

    updating the app page and adding the code page

--- js/side.js
+++ js/side.js
@@ -1,13 +1,23 @@
-function ReverseString()
+function GetValue()
 {
-    //get initial string
     let word = document.getElementById("tboxreverse").value;
-    let newWord = "";
+    let reversedWord = ReverseString(word);
+    DisplayValue(reversedWord);
 
+}
+
+function ReverseString(word)
+{
+    let newWord = "";
     for (let index = word.length - 1; index >= 0; index--)
     {
         newWord += word[index];
     }
-    
+    return newWord;
+}
 
+function DisplayValue(word)
+{
+    document.getElementById("msg").innerHTML = word;
+    document.getElementById("alert").classList.remove("invisible");
 }
