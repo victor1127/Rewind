0dfc4ef9ce018a81a9e2cff100741bfa4159efc4
victor1127 <95936106+victor1127@users.noreply.github.com>
2022-01-31 23:22:19 -0400

    Adding js functions

--- js/side.js
+++ js/side.js
@@ -1,4 +1,13 @@
-function DisplayHelloWorld()
+function ReverseString()
 {
-    alert("Hello world");
+    //get initial string
+    let word = document.getElementById("tboxreverse").value;
+    let newWord = "";
+
+    for (let index = word.length - 1; index >= 0; index--)
+    {
+        newWord += word[index];
+    }
+    
+
 }
